0ec258c57655a97224210b60f47ae0a162f40c88
Kenny <kenny2061@gmail.com>
2026-06-25 14:40:49 +0800

    Update devopsdays2026 build_code_script.sh

--- devopsdays2026/web/scripts/build_code_script.sh
+++ devopsdays2026/web/scripts/build_code_script.sh
@@ -2,5 +2,8 @@
 set -e          # 遇到任何錯誤立即停止執行
 set -o pipefail # 捕捉 pipeline 中間的錯誤
 
+# Example if the web app requires Linux specific features
+go env -w GOOS=linux
+
 
 go build -gcflags="all=-N -l" -buildvcs=false -o $OUTPUT_FOLDER/web-app $SOURCE_FOLDER
